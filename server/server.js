9936ad8819fdd1cd5c25481f389fdaf9e82bde82
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-11-20 18:49:25 +0530

    [Feature] installed cors module to fix the cors issue which facing for different urls.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 
 const PORT = 3001;
 
 // Import routes here
 const api = require("./routes/api");
 
-
-
 // created instance of express.
 const app = express();
 
+// user cors middleware to handle cors error
+app.use(cors())
 // use bodyparser to handle json data
 app.use(bodyParser.json());
 
