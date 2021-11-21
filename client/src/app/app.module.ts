8df549baffab1dd23ffb4d0cb6ca9f2337343276
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-11-21 17:51:17 +0530

    - [Feature] In app.module.ts file imported some services and packages and stored in the providers.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { EventService } from './services/event.service';
+import { AuthGuard } from './auth.guard';
+import { AngularInterceptorService } from './services/angular-interceptor.service'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,12 @@ import { EventService } from './services/event.service';
     FormsModule,
     HttpClientModule
   ],
-  providers: [AuthService, EventService],
+  providers: [AuthService,AuthGuard, EventService,
+  {
+    provide:HTTP_INTERCEPTORS,
+    useClass:AngularInterceptorService,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
